489e3a031ecd1e14d52d14b92d8407c87844955d
Akshay0503 <akshaybhalerao0503@gmail.com>
2018-04-22 13:39:07 +0530

    dplyr case study

--- dplyr.R
+++ dplyr.R
@@ -1,3 +1,83 @@
+#READFILE: METHOD2
+sales2=read.csv(file.choose())
+str(sales2)
+
+
 #deployer
-df=read.csv('./data/denco.csv')
-df
+sales=read.csv('./data/denco.csv')
+sales
+str(sales)
+head(sales) #top 6 rows
+unique(sales$custname) #how much unique names in data
+length(unique(sales$custname))
+#dplyr
+names(sales)
+library(dplyr)
+names(sales)
+sales %>% group_by(custname) %>% summarize(Revenue=sum(revenue)) %>% arrange(desc(Revenue))
+#TRIUMPH iNSULATION GAVE MAXIMUM REVENUE
+options(tibble.width= Inf) #display all colloms
+options
+df3=sales
+df3[15:60]
+
+library(dplyr)
+sales %>% count(custname, sort=TRUE) %>% head(n=5)
+
+sales %>% dplyr::group_by(custname) %>% dplyr::summarise(n= n()) %>% dplyr::arrange(desc(n)) %>% head(n=5)
+
+#summarise by part NUm
+
+sales %>% dplyr::group_by(partnum) %>% dplyr::summarise(n= n()) %>% dplyr::arrange(desc(n)) %>% head(n=5)
+ 
+#which parts have highest profit: partno- sum(profit)
+
+names(sales)
+sales %>% group_by(partnum) %>% summarise(Margin=  sum(margin)) %>%
+  arrange(desc(Margin)) %>% head()
+#filter 
+sales %>% filter(region=='01-East') %>% head()
+ 
+df4=sales %>% group_by(region) %>% summarise(Revenue= sum(revenue)) %>% arrange(desc(Revenue))
+df4
+pie(x=df4$Revenue, col=1:2)
+barplot(df4$Revenue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
